3c47c35ea4711bc352ad0679346c923ee77f2e0d
Ivan Sentemon <sentemon.i@gmail.com>
2024-02-13 09:12:38 +0100

    Revert "#"
    This reverts commit 73ef6c08c12f223eaf99d4dbbc4f2bbcde0f4303.

--- leetcode/firstPalindrome.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def firstPalindrome(self, words: list[str]) -> str:
-        for i in words:
-            if i[::] == i[::-1]:
-                return i
-        
-        return ""
